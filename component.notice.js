ba7e70de5020b25e4b9e059ce089aa761cd4c9e6
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2021-08-12 14:50:06 +0300

    local-prop-value

--- component.notice.js
+++ component.notice.js
@@ -6,12 +6,26 @@ export var ComponentNotice = {
 		data: Array,
 		data2: Object,
 	},
+	data: function () {
+		return {
+			message: this.thisIsMessage
+		};
+	},
+	created: function () {
+	    setTimeout(() => {
+	    	this.message += ' <b>' + this.message + '</b>';
+	    }, 1000);
+	    setTimeout(() => {
+	    	this.message += ' <i>' + this.message + '</i>';
+	    }, 3000);
+	},
 	template: `
 		<div style="color: red; background-color:yellow;"
 			:style="{ fontSize: fontSize + 'px', data2 }"
 			:class="data2"
 		>
-			{{ thisIsMessage }} {{ data.join('') }}
+			<span v-html="message"></span>
+			{{ data.join('') }}
 		</div>
 	`
 };
