f0c668de8546d6b071a64d0ae5bcdd6184b4d2a1
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2023-12-25 14:08:14 +0300

    vue3 test, setup api

--- index.php
+++ index.php
@@ -6,11 +6,109 @@ use Bitrix\Main\UI\Extension;
 
 Extension::load('ui.vue3');
 
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script/type/importmap
+//		"ui.vue3": "/bitrix/js/ui/vue3/vue/prod/dist/vue.bundle.min.js"
 ?>
 
-<?php if (0) { ?>
+<h3>App1</h3>
 
+<div id="application1">-1</div>
+
+<script type="text/x-template" id="app-template1">
+	<div>
+		Counter: {{ counter }}
+	</div>
+	<p>
+		<ButtonCounter/> | <ButtonCounter2/>
+	</p>
+</script>
+
+<script type="module">
+
+	//const BitrixVue = BX.Vue3.BitrixVue;
+	const {
+		BitrixVue,
+		ref,
+		//h,
+		//reactive
+	} = BX.Vue3;
+
+	const ButtonCounter = {
+		data()
+		{
+			return {
+				count: 0
+			}
+		},
+		// language=Vue
+		template: '<button @click="count++">—чЄтчик кликов Ч {{ count }}</button>'
+	};
+
+	const ButtonCounter2 = BitrixVue.mutableComponent('ui-button-counter', {
+		data()
+		{
+			return {
+				count: 0
+			}
+		},
+		// language=Vue
+		template: '<button @click="count++">—чЄтчик кликов Ч {{ count }}</button>'
+	});
+
+	//console.log(h, ref, reactive);
+
+	BitrixVue.createApp({
+		components: {
+			ButtonCounter,
+			ButtonCounter2,
+		},
+
+		// new
+		setup() {
+			const counter = ref(0)
+
+			const increment = () => counter.value++
+
+			setInterval(increment, 1000)
+
+			return {
+				counter
+			}
+		},
+
+		// old
+		/*
+		data()
+		{
+			return {
+				counter: 0
+			}
+		},
+		mounted()
+		{
+			setInterval(() => {
+				this.counter++
+			}, 1000)
+		},
+		*/
+
+		template: '#app-template1'
+
+	}).mount('#application1');
+
+</script>
+
+<hr>
+
+<h3>App2</h3>
 <div id="application"></div>
+<script type="importmap">
+{
+	"imports": {
+		"ui.vue3": "/bitrix/js/ui/vue3/vue/prod/src/vue.js"
+	}
+}
+</script>
 <script type="module">
 import {BitrixVue} from 'ui.vue3';
 BitrixVue.createApp({
@@ -31,38 +129,6 @@ BitrixVue.createApp({
 		Counter: {{ counter }}
 	`
 }).mount('#application');
-</script>
-
-<?php } ?>
-
-
-<div id="application2">-1</div>
-
-<script type="text/x-template" id="app-template2">
-	<div>
-		Counter: {{ counter }}
-	</div>
-</script>
-
-<script type="module">
-	const BitrixVue = BX.Vue3.BitrixVue;
-
-	BitrixVue.createApp({
-		data()
-		{
-			return {
-				counter: 0
-			}
-		},
-		mounted()
-		{
-			setInterval(() => {
-				this.counter++
-			}, 1000)
-		},
-		template: '#app-template2'
-	}).mount('#application2');
-
 </script>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
